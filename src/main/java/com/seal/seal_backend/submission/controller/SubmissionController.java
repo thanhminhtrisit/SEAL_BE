6818ff56c59c550be50528bac7db5fb67ceccde0
Haisura <vyastaroth123@gmail.com>
2026-06-17 00:38:12 +0700

    feat(submission): add submission creation

--- src/main/java/com/seal/seal_backend/submission/controller/SubmissionController.java
+++ src/main/java/com/seal/seal_backend/submission/controller/SubmissionController.java
@@ -1,10 +1,14 @@
 package com.seal.seal_backend.submission.controller;
 
 import com.seal.seal_backend.common.api.ApiResponse;
+import com.seal.seal_backend.submission.dto.request.CreateSubmissionRequestDTO;
+import com.seal.seal_backend.submission.dto.response.SubmissionResponseDTO;
+import com.seal.seal_backend.submission.service.SubmissionService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Flow: Project submission tracking
@@ -15,10 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/submissions")
 @Tag(name = "Submission Tracking")
+@RequiredArgsConstructor
 public class SubmissionController {
+
+    @Autowired
+    private final SubmissionService submissionService;
 
     @GetMapping("/ping")
     public ApiResponse<String> ping() {
         return ApiResponse.ok("Submission Tracking module is alive");
+    }
+
+    @PostMapping
+    public ResponseEntity<SubmissionResponseDTO> createSubmission(
+            @RequestBody CreateSubmissionRequestDTO request,
+            @RequestParam Long userId
+    ) {
+        return ResponseEntity.ok(
+                submissionService.createSubmission(request, userId)
+        );
     }
 }
