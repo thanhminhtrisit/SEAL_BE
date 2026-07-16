b7579cd3d8c819e6239d6735922b386f7f859c0b
VibeCodingGuy <nguyencongthienan1@gmail.com>
2026-07-17 01:01:15 +0700

    chore(Notificatio): fix bug in log

--- src/main/java/com/seal/seal_backend/SealBackendApplication.java
+++ src/main/java/com/seal/seal_backend/SealBackendApplication.java
@@ -2,10 +2,12 @@ package com.seal.seal_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class SealBackendApplication {
 
 	public static void main(String[] args) {
